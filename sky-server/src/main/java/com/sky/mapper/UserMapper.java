e7bb5adf30b7a8774eae4c5dd3010d894ae2a923
qmtzdsta <1686296540@qq.com>
2023-07-17 19:56:30 +0800

    微信支付部分，未学习

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -9,5 +9,8 @@ public interface UserMapper {
     @Select("select * from user where openid = #{openid}")
     User findByOpenId(String openid);
 
+    @Select("select * from user where id = #{id}")
+    User findById(Long id);
+
     void insert(User user);
 }
